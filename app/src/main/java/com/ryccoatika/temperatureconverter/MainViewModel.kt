0c60ecb5a75f2fba4cc60e26f8ffda15a890ec86
ryccoatika <ryccoatika.ra@gmail.com>
2021-01-19 23:33:28 +0700

    would change the scale state

--- app/src/main/java/com/ryccoatika/temperatureconverter/MainViewModel.kt
+++ app/src/main/java/com/ryccoatika/temperatureconverter/MainViewModel.kt
@@ -5,4 +5,12 @@ import androidx.lifecycle.ViewModel
 class MainViewModel : ViewModel() {
     val scale = MutableLiveData<TemperatureScale>()
 
+    fun setScale(stringRes: Int) {
+        when(stringRes) {
+            R.string.celsius -> scale.postValue(TemperatureScale.Celcius)
+            R.string.fahrenheit -> scale.postValue(TemperatureScale.Fahrenheit)
+            R.string.kelvin -> scale.postValue(TemperatureScale.Kelvin)
+            else -> scale.postValue(TemperatureScale.Celcius)
+        }
+    }
 }
