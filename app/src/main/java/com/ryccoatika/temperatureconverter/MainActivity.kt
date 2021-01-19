a55b01794ec43307b242c24775061fb497b776c0
ryccoatika <ryccoatika.ra@gmail.com>
2021-01-19 23:28:58 +0700

    define scalechooserdialog as material alert dialog builder

--- app/src/main/java/com/ryccoatika/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/ryccoatika/temperatureconverter/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var scaleChooserDialog: MaterialAlertDialogBuilder
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
